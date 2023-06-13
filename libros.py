e477fffe5fa16da3ec82267f3b2ad8e7ea743d22
CamiloCarabajal <ultrakin3t@gmail.com>
2023-06-12 21:06:56 -0300

    Funciones para el menu principal

--- libros.py
+++ libros.py
@@ -26,19 +26,24 @@ class ProgramaPrincipal:
 
             if opcion > 5 or opcion < 0:
                 print("ERROR : Por favor, ingrese un número entre 0 y 5")
-            else:
-                if opcion == 1:
+            elif opcion == 1:
                     self.cargarLibros()
+            elif opcion == 2:
+                isbm_buscado: input("Ingrese el isbm del libro que desea modificar: ")
+                nuevo_precio: input("Ingrese el nuevo precio: ")
+                self.modificar_libro(isbm_buscado, nuevo_precio)
 
+                                   
+        
     def cargarLibros(self):
         print("hola desde cargar libros")
-        isbm = input("Ingrese ISBM")
-        titulo = input("Ingrese titulo")
-        autor = input("Ingrese autor")
-        genero = input("Ingrese género")
-        precio = float(input("Ingrese precio"))
-        fechaUltimoPrecio = input("Ingrese la fecha del ultimo precio (dd/mm/aaaa)")
-        cantidadDisponible = int(input("Ingrese la cantidad disponible del libro"))
+        isbm = input("Ingrese ISBM: ")
+        titulo = input("Ingrese titulo: ")
+        autor = input("Ingrese autor: ")
+        genero = input("Ingrese género: ")
+        precio = float(input("Ingrese precio: "))
+        fechaUltimoPrecio = input("Ingrese la fecha del ultimo precio (dd/mm/aaaa): ")
+        cantidadDisponible = int(input("Ingrese la cantidad disponible del libro: "))
 
         miConexion = Conexiones()
         miConexion.abrirConexion()
@@ -63,7 +68,53 @@ class ProgramaPrincipal:
         miConexion.conexion.commit()
         miConexion.cerrarConexion()
 
+    def modificar_libro(self, isbm_buscado, nuevo_precio):
+        miConexion = Conexiones()
+        miConexion.abrirConexion()
+        try:
+            miConexion.cursor.execute(
+                 "UPDATE LIBROS SET precio = ? WHERE ISBN = ?",
+                (nuevo_precio, isbm_buscado),
+            )
+            miConexion.miConexion.commit()
+            print("Libro modificado correctamente")
+        except Exception as err:
+            print("Error al modificar un libro")
+            print(err)
+        finally:
+            miConexion.cerrarConexion()
 
+    def borrar_libro(self, isbm_buscado):
+        miConexion = Conexiones()
+        miConexion.abrirConexion()
+        try:
+             miConexion.cursor.execute(
+                "DELETE FROM LIBROS WHERE ISBN = ?",
+                (isbm_buscado),
+             )
+             miConexion.miConexion.commit()
+             print("Libro eliminado correctamente")
+        except:
+            print("Error al eliminar el libro")
+        finally:
+            miConexion.cerrarConexion()
+
+    def modificar_cant(self, isbm_buscado, nueva_cantidad):
+        miConexion = Conexiones()
+        miConexion.abrirConexion()
+
+        try:
+            miConexion.cursor.execute(
+            "UPDATE LIBROS SET CantDisponible = ? WHERE ISBN = ?",
+                (nueva_cantidad, isbm_buscado),
+            )
+            print("La cantidad disponible ha sido actualizada ")
+        except:
+            print("Hubo un error al actualizar la cantidad nueva")
+        finally:
+            miConexion.cerrarConexion()
+
+    #def mostrar_libros(self): Calculo que es un for seleccionando lo de la tabla
 
 programa = ProgramaPrincipal()
 programa.crearTabla()
